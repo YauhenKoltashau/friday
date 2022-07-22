50fd8c163516c459bf83905d2c31503744fd5fc9
YauhenKoltashau <yauhen.koltashau.1985@gmail.com>
2022-07-22 21:13:17 +0300

    commit 1 Profile nameYauhen

---
+++ friday1/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import {Header} from "./components/Header/Header";
+import {RoutesBlock} from "./components/RoutesBlock/RoutesBlock";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <Header/>
+        <div>Profile</div>
+      <RoutesBlock/>
+    </div>
+  );
+}
+
